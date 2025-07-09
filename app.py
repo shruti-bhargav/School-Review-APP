0827492d84bbc721ec588993ebc5cb6a757bef0e
shruti Bhargav <bhargavshruti67@gmail.com>
2025-07-09 21:25:56 +0530

    Final updates after debugging

--- app.py
+++ app.py
@@ -1,41 +1,45 @@
-from flask import Flask , render_template , request , redirect
+
+
+
+from flask import Flask, render_template, request, redirect
 import mysql.connector
 import config
 
-app= Flask(__name__)
-db=mysql.connector.connect(
-    host=config.DB_HOST ,
-    user=config.DB_USER ,
+app = Flask(__name__)
+
+# Database connection
+db = mysql.connector.connect(
+    host=config.DB_HOST,
+    user=config.DB_USER,
     password=config.DB_PASSWORD,
     database=config.DB_NAME
 )
-
-cursor=db.cursor()
+cursor = db.cursor()
 
 @app.route('/')
 def home():
-    return render_template("add_review_html")
+    return render_template("add_review.html", all_reviews=[])
 
-@app.route('/submit/' , methods=["post"])
+@app.route('/submit/', methods=["POST"])
 def submit():
-    school=request.form['school']
-    rating=request.form['rating']
-    review=request.form['review']
+    school = request.form['school']
+    rating = request.form['rating']
+    review = request.form['review']
 
-
-    query=" INSERT INTO reviews(school_name,rating,review_text) VALUESA (%s,%s,%s)"
-    values=(school,rating,review)
-    cursor.execute(query,values)
+    query = "INSERT INTO reviews (school_name, rating, review_text) VALUES (%s, %s, %s)"
+    values = (school, rating, review)
+    cursor.execute(query, values)
     db.commit()
+    print("Form submitted")
+    return redirect('/review/')
 
+@app.route('/review/')
+def reviews():
+    cursor.execute("SELECT * FROM reviews")
+    all_reviews = cursor.fetchall()
+    return render_template("add_review.html", all_reviews=all_reviews)
 
-    @app.route('/review/')
-    def reviews():
-        cursor.execute("SELECT * FROM reviews")
-        all_reviews=cursor.fetchall()
-        return render_template("reviews.html")
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
 
 
