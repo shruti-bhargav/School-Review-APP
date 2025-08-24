fb961d4f87df7cef6f8321e03f481619d3f2d191
shruti Bhargav <bhargavshruti67@gmail.com>
2025-08-25 01:05:13 +0530

    updated app.py

--- app.py
+++ app.py
@@ -12,12 +12,13 @@ port=int(os.environ.get("PORT", "5000"))
 # --- Database Connection ---
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.environ.get("MYSQLHOST"),
-        user=os.environ.get("MYSQLUSER" )     
-        password=os.environ.get("MYSQLPASSWORD" ),       
-        database=os.environ.get("MYSQLDATABASE" ),
-        port=int(os.environ.get("MYSQLPORT" ))
+        host=os.environ.get("MYSQLHOST", "localhost"),
+        user=os.environ.get("MYSQLUSER", "root"),
+        password=os.environ.get("MYSQLPASSWORD", "shruti"),
+        database=os.environ.get("MYSQLDATABASE", "school_reviews"),
+        port=int(os.environ.get("MYSQLPORT", 3306))
     )
+
 
 # --- Home Page / List All Reviews ---
 @app.route('/')
