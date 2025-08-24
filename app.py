10ee42e0dcdedd84410bce6c9c1516426083a757
shruti Bhargav <bhargavshruti67@gmail.com>
2025-08-25 00:53:30 +0530

    updated app.py

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ port=int(os.environ.get("PORT", "5000"))
 # --- Database Connection ---
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.environ.get("MYSQLHOST" , "localhost"),
-        user=os.environ.get("MYSQLUSER" , "root"),       
-        password=os.environ.get("MYSQLPASSWORD" , "shruti"),       
-        database=os.environ.get("MYSQLDATABASE" , "school_reviews"),
-        port=int(os.environ.get("MYSQLPORT" , "3306"))
+        host=os.environ.get("MYSQLHOST"),
+        user=os.environ.get("MYSQLUSER" )     
+        password=os.environ.get("MYSQLPASSWORD" ),       
+        database=os.environ.get("MYSQLDATABASE" ),
+        port=int(os.environ.get("MYSQLPORT" ))
     )
 
 # --- Home Page / List All Reviews ---
